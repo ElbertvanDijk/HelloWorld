53bac62acd4f96977fcd5e100902e3a5525f5924
Admin <admin@localhost.localdomain>
2016-11-23 17:11:47 +0100

    Added Swagger documentation

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,17 +1,20 @@
 package hello;
 
+import io.swagger.annotations.ApiOperation;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hallo, %s!";
+    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @ApiOperation(value = "name", nickname = "Greeting", tags = {"Greeting", "Get"})
+    @RequestMapping(method = RequestMethod.GET, path = "/greeting", produces = {"application/json"})
     public Greeting greeting(@RequestParam(value="name", defaultValue="Wereld") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
